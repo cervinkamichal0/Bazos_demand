97b34949a7b57b9a02698aac7234f93717f95ba3
Michal Červinka <cerm47@vse.cz>
2024-02-08 22:07:22 +0100

    Přidání parametrů cenaOd a cenaDo do metody scrapeBazos. Pridani inputu argumentu z txt souboru.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,37 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Integer.valueOf;
+
 
 public class Main {
     public static void main(String[] args) {
+        try {
 
-        List<Inzerat> inzeraty = Scraper.scrapeBazos("bmw+e46");
-        for (Inzerat inzerat : inzeraty) {
-            System.out.println(inzerat.nadpis);
+            URL url = Main.class.getResource("config.txt");
+            File config = new File(url.getPath());
+            Scanner myReader = new Scanner(config);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+                String[] rozdelenyData = data.split(";");
+                List<Inzerat> inzeraty = Scraper.scrapeBazos(rozdelenyData[0], Integer.parseInt(rozdelenyData[1]), Integer.parseInt(rozdelenyData[2]));
+                System.out.println(rozdelenyData[0] + ": " + inzeraty.size());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Nebyl nalezen soubour config.txt");
+            e.printStackTrace();
         }
+        //List<Inzerat> inzeraty = Scraper.scrapeBazos("bmw+e46",50000,100000);
+        //for (Inzerat inzerat : inzeraty) {
+        //    System.out.println(inzerat.nadpis);
+        //}
 
     }
 }
--- src/main/java/org/example/Scraper.java
+++ src/main/java/org/example/Scraper.java
@@ -14,12 +14,14 @@ import java.util.regex.Matcher;
 import static java.lang.Integer.valueOf;
 
 public class Scraper {
-    public static List<Inzerat> scrapeBazos(String searchTerm) {
-        String scrapeUrl = "https://www.bazos.cz/search.php?hledat=" + searchTerm + "&rubriky=www&hlokalita=&humkreis=25&cenaod=&cenado=&Submit=Hledat&order=&kitx=ano";
-
+    public static List<Inzerat> scrapeBazos(String searchTerm, int cenaOd, int cenaDo) {
+        String scrapeUrl = "https://www.bazos.cz/search.php?hledat=" + searchTerm + "&rubriky=www&hlokalita=&humkreis=25&cenaod="+cenaOd+"&cenado=" + cenaDo + "&Submit=Hledat&order=&kitx=ano";
+        if (cenaOd>cenaDo){
+            throw new IllegalArgumentException("Cena od musi byt mensi nez cena do");
+        }
         try{
             Document html = Jsoup.connect(scrapeUrl).get();
-            Elements inzeraty;
+            Elements inzeraty=html.select(".inzeraty");
 
             String pocetInzeratuText = html.select(".inzeratynadpis").text();
             pocetInzeratuText = pocetInzeratuText.replaceAll("\\s","");
@@ -40,9 +42,20 @@ public class Scraper {
             List<Inzerat> inzeratyList = new ArrayList<>();
             Inzerat bazosInzerat;
 
+            for(Element inzerat: inzeraty){
+                nadpis = inzerat.select(".nadpis").text();
+                popis = inzerat.select(".popis").text();
+                cena = inzerat.select(".inzeratycena").text();
+                lokace = inzerat.select(".inzeratylok").text();
+                datumVlozeni = inzerat.select(".velikost10").text();
+                img = inzerat.select(".obrazek").attr("src");
+                url = inzerat.select("a").attr("href");
+                bazosInzerat = new Inzerat(nadpis, popis, cena, lokace, datumVlozeni, img, url);
+                inzeratyList.add(bazosInzerat);
+            }
 
             for (int i = 1; i<= pocetStranek; i++){
-                scrapeUrl = "https://www.bazos.cz/search.php?hledat=" + searchTerm + "&hlokalita=&humkreis=25&cenaod=&cenado=&order=&crz=" + i*20;
+                scrapeUrl = "https://www.bazos.cz/search.php?hledat=" + searchTerm + "&hlokalita=&humkreis=25&cenaod="+cenaOd +"&cenado="+cenaDo+"&order=&crz=" + i*20;
 
                 html = Jsoup.connect(scrapeUrl).get();
                 inzeraty = html.select(".inzeraty");
