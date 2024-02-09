a4e3c46eeafb20e8f8be9c30fda123107f9d202c
Michal Červinka <cerm47@vse.cz>
2024-02-09 17:18:15 +0100

    Vytvoření DiscordBota. Přidání metod getBot, sendImage, getImageURL.

---
+++ src/main/java/org/example/DiscordBot.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
+import net.dv8tion.jda.api.requests.ErrorResponse;
+import net.dv8tion.jda.api.utils.FileUpload;
+
+
+import java.awt.image.DataBufferByte;
+import java.io.File;
+
+import static java.lang.Thread.sleep;
+
+public class DiscordBot {
+    private static JDA getBot() {
+        final JDA bot = JDABuilder.createDefault("MTIwNTUwMjgyMTYzODg3MzE0OA.GLh6Rm.ClrxTruBsQMEvvM59nnOOfzn-UkG21zwNZsvYQ").setActivity(Activity.watching("Bazos")).build();
+        return bot;
+    }
+    public static String sendImage(String message) {
+        JDA bot = getBot();
+        try {
+            bot.awaitReady();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+            FileUpload img = FileUpload.fromData(new File("D:\\java\\scraper_2.0\\src\\main\\resources\\test.jpg"), "image.png");
+            TextChannel textChannel = bot.getTextChannelById("1063949035054051409");
+            textChannel.sendFiles(img).queue();
+
+            String imgURL=getImageURL();
+            return imgURL;
+    }
+    private static String getImageURL() {
+        JDA bot = getBot();
+
+        try {
+            bot.awaitReady();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        TextChannel textChannel = bot.getTextChannelById("1063949035054051409");
+        String latestID = textChannel.getLatestMessageId();
+        textChannel.retrieveMessageById(latestID).queue((message) -> {
+            message.addReaction(Emoji.fromUnicode("U+2714")).queue();
+            String imgURL = message.getAttachments().get(0).getUrl();
+            System.out.println(imgURL);
+
+
+        }, new ErrorHandler().handle(ErrorResponse.UNKNOWN_MESSAGE, (e) -> {
+            throw new RuntimeException(e);
+        }));
+        return null;
+
+    }
+    //JDA bot = JDABuilder.createDefault("MTIwNTUwMjgyMTYzODg3MzE0OA.GLh6Rm.ClrxTruBsQMEvvM59nnOOfzn-UkG21zwNZsvYQ").setActivity(Activity.watching("Bazos")).build();
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,9 @@ import static java.lang.Integer.valueOf;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-
+        String url = DiscordBot.sendImage("test");
+        System.out.println(url);
+        /*try {
             URL url = Main.class.getResource("config.txt");
             File config = new File(url.getPath());
             Scanner myReader = new Scanner(config);
@@ -21,17 +22,16 @@ public class Main {
                 System.out.println(data);
                 String[] rozdelenyData = data.split(";");
                 List<Inzerat> inzeraty = Scraper.scrapeBazos(rozdelenyData[0], Integer.parseInt(rozdelenyData[1]), Integer.parseInt(rozdelenyData[2]));
+                for (Inzerat inzerat : inzeraty) {
+
+                }
                 System.out.println(rozdelenyData[0] + ": " + inzeraty.size());
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Nebyl nalezen soubour config.txt");
             e.printStackTrace();
-        }
-        //List<Inzerat> inzeraty = Scraper.scrapeBazos("bmw+e46",50000,100000);
-        //for (Inzerat inzerat : inzeraty) {
-        //    System.out.println(inzerat.nadpis);
-        //}
+        }*/
 
     }
 }
